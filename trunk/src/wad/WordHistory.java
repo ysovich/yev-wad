8f0e30035ff42c993e2613630972f610ed4141d8
yev.sovich@gmail.com <yev.sovich@gmail.com@463428aa-b319-32de-5ec8-4cc5f0784196>
2013-12-14 19:57:02 +0000

    major change, save files only on exit

--- trunk/src/wad/WordHistory.java
+++ trunk/src/wad/WordHistory.java
@@ -1,8 +1,9 @@
 package wad;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WordHistory {
 	public Word word;
-	public List<Attempt> attemptList;
+	public List<Attempt> attemptList = new ArrayList<Attempt>();;
 }
