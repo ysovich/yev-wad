dc0e31243239db21b4562d7c140b5b7d86622d0b
yev.sovich@gmail.com <yev.sovich@gmail.com@463428aa-b319-32de-5ec8-4cc5f0784196>
2013-12-15 22:49:14 +0000

    even bigger change, changed domain model and file format

--- trunk/src/wad/Attempt.java
+++
@@ -1,8 +0,0 @@
-package wad;
-
-import java.util.Date;
-
-public class Attempt {
-	public String wordTitle;
-	public Date date;
-}
--- trunk/src/wad/Word.java
+++ trunk/src/wad/Word.java
@@ -1,8 +1,12 @@
 package wad;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Word {
 	public String title;
 	public String definition;
 	public String example;
 	public boolean isNew;
+	public ArrayList<Date> attemptList = new ArrayList<Date>();
 }
